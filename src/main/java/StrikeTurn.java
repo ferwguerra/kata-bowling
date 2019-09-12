c3775bd2921431ee9bdec8648e777dd1e1e19554
Fernando Guerra <fernandoguerra@Fernandos-MacBook-Pro.local>
2019-09-12 15:31:17 +0200

    first step of extra ball

--- src/main/java/StrikeTurn.java
+++ src/main/java/StrikeTurn.java
@@ -4,21 +4,31 @@ public class StrikeTurn extends Turn {
     private Character nextSecondBall;
 
     public StrikeTurn(Turn nextTurn, Turn nextNextTurn) {
-        this.nextFirstBall = nextTurn.firstBall;
-        if (nextTurn.secondBall != null) {
-            this.nextSecondBall = nextTurn.secondBall;
-        } else {
-            nextSecondBall = nextNextTurn.firstBall;
+        if(nextTurn != null) {
+            this.nextFirstBall = nextTurn.firstBall;
+            if (nextTurn.secondBall != null) {
+                this.nextSecondBall = nextTurn.secondBall;
+            } else {
+                nextSecondBall = nextNextTurn.firstBall;
+            }
         }
     }
 
     public int getScore() {
         int score = DEFAULT_SCORE_SPARE_OR_STRIKE;
-        if (nextSecondBall == '/') {
-            score += DEFAULT_SCORE_SPARE_OR_STRIKE;
-        } else {
-            score += getBallValue(nextFirstBall) + getBallValue(nextSecondBall);
+
+        if(!isExtraBall()) {
+            if (nextSecondBall == '/') {
+                score += DEFAULT_SCORE_SPARE_OR_STRIKE;
+            } else {
+                score += getBallValue(nextFirstBall) + getBallValue(nextSecondBall);
+            }
         }
+
         return score;
+    }
+
+    private boolean isExtraBall() {
+        return nextFirstBall == null;
     }
 }
